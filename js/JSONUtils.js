046a6ff5fbef1b24e8008cc7af736690c60bad6e
Ed Lee <elee@s2kfan.dhcp.lbl.gov>
2012-05-14 15:03:59 -0700

    Added JSONUtils.createJBrowseSequenceAlteration()

--- js/JSONUtils.js
+++ js/JSONUtils.js
@@ -179,6 +179,44 @@ JSONUtils.createJBrowseFeature = function(arep, afeature, classIndex)  {
     return jfeature;
 };
 
+/**
+*  creates a sequence alteration in JBrowse JSON format
+*  takes as arguments:
+*      arep: ArrayRepr for kind of JBrowse feature to output
+*      afeature: sequence alteration in ApolloEditorService JSON format,
+*/
+JSONUtils.createJBrowseSequenceAlteration = function(arep, afeature)  {
+    var loc = afeature.location;
+    var uid = afeature.uniquename; 
+
+    var classIndex = 0;
+    var jfeature = new Array();
+
+    jfeature.uid = uid;
+
+    // need ArrayRepr constructor that sets class field:
+    //    var jfeature = arep.constructFeature(classIndex);
+    // or probably better:
+    //    var jfeature = arep.constructFeature(className);
+    // 
+    jfeature[0] = classIndex;
+    arep.set(jfeature, "Start", loc.fmin);
+    arep.set(jfeature, "End", loc.fmax);
+    arep.set(jfeature, "Strand", loc.strand);
+    if (arep.hasDefinedAttribute(jfeature, "Id")) {
+    	arep.set(jfeature, "Id", uid);
+    }
+    if (arep.hasDefinedAttribute(jfeature, "Type"))  {
+    	var type = afeature.type.name;
+    	arep.set(jfeature, "Type", type);
+    }
+    if (arep.hasDefinedAttribute(jfeature, "Residues"))  {
+    	var residues = afeature.residues;
+    	arep.set(jfeature, "Residues", residues);
+    }
+    return jfeature;
+};
+
 
 /** 
 *  creates a feature in ApolloEditorService JSON format
