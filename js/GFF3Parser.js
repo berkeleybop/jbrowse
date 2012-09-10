02426e17f43e98b1e4431b2815ec2f04abcabf17
Justin Reese <justaddcoffee+github@gmail.com>
2012-09-10 15:50:19 +0700

    added 4 tests for GFF3 parsing - two are failing because I am having trouble reading fixture files, will fix soon.

--- js/GFF3Parser.js
+++ js/GFF3Parser.js
@@ -8,25 +8,48 @@ Group1.33	maker	exon	245702	245879	.	+	.	ID=1:gnomon_566853_mRNA:exon:5977;Paren
 
 and returns a JSON data structure like this:
 
-[
-  {"parent":
-    [Group1.33","maker","gene","245454","247006",".","+",".","ID=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=maker-Group1%252E33-pred_gff_GNOMON-gene-4.137"],
-  "children": 
-     [{"parent": [Group1.33","maker","mRNA","245454","247006",".","+",".","ID=1:gnomon_566853_mRNA;Parent=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=gnomon_566853_mRNA;_AED=0.45;_eAED=0.45;_QI=138|1|1|1|1|1|4|191|259"],
-      "children": [
-             [Group1.33","maker","exon","245454","245533",".","+",".","ID=1:gnomon_566853_mRNA:exon:5976;Parent=1:gnomon_566853_mRNA"],
-             [Group1.33","maker","exon","245702","245879",".","+",".","ID=1:gnomon_566853_mRNA:exon:5977;Parent=1:gnomon_566853_mRNA"]
-          ]
-     ]
-   },
-   ... next parent/child/descendants, e.g. gene/mRNA/exons or whatever ...
+[{
+"parent":
+  ["Group1.33","maker","gene","245454","247006",".","+",".","ID=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=maker-Group1%252E33-pred_gff_GNOMON-gene-4.137"],
+"children": 
+  [{
+    "parent": 
+    ["Group1.33","maker","mRNA","245454","247006",".","+",".","ID=1:gnomon_566853_mRNA;Parent=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=gnomon_566853_mRNA;_AED=0.45;_eAED=0.45;_QI=138|1|1|1|1|1|4|191|259"],
+   "children": [
+      ["Group1.33","maker","exon","245454","245533",".","+",".","ID=1:gnomon_566853_mRNA:exon:5976;Parent=1:gnomon_566853_mRNA"],
+      ["Group1.33","maker","exon","245702","245879",".","+",".","ID=1:gnomon_566853_mRNA:exon:5977;Parent=1:gnomon_566853_mRNA"]
+    ]}]
+ }
+ ... next parent/child/descendants, e.g. gene/mRNA/exons or whatever ...
 ]
 
 Created by Justin Reese 2012
 justaddcoffee@gmail.com
 */
 
-function GFF3toJson(gffFileURL) {
-    return false;
+function GFF3toJson() {
 }
+GFF3toJson.prototype.parse = function(gff3File) {
+    // this method uses fileReader, which requires
+    // Firefox >= 3.6 || Chrome >= 7 || IE >= 10
+
+    this.gff3File = gff3File; 
+    if ( gff3File ) {
+	var r = new FileReader();
+	var reader=new FileReader();
+	/*r.onload = function(e) { 
+	  var contents = e.target.result;
+	  console.log( "Got the file.n"  + "name: " + gff3File.name + "n" + "type: " + gff3File.type + "n"  +"size: " + gff3File.size + " bytesn"
+	  + "starts with: " + contents.substr(1, contents.indexOf("n"))
+	  );  
+	  }
+	*/
+	reader.readAsText( gff3File, "UTF-8" );
+	console.log("here's what's in the file: " + reader.result)
+	null;
+    } else { 
+	alert("Failed to load file");
+    }
+    
+};
 
--- tests/js_tests/webapollo_specs/GFF3ParserSpec.js
+++ tests/js_tests/webapollo_specs/GFF3ParserSpec.js
@@ -1,64 +1,45 @@
-describe("GFF3Parser", function() {
-  var player;
-  var song;
+describe("GFF3toJson", function() { 
+	// GFF3toJson takes a GFF3 URL and converts it to an array of hash refs where each
+	// hash has a "parent" key/value pair and zero or more "children" key/value pairs, 
+	// and the children in turn can have more parent/children. 
+	var gff3Parser;
+	var makerGff3;
+	var makerCorrectJsonFile;
+	
+	beforeEach(function() {
+		jasmine.getFixtures().fixturesPath = './fixtures/';
+		gff3Parser = new GFF3toJson();		
+		makerGff3 = '/Users/jtr4v/projects/apollo-web/apollo-web_jbrowse/tests/js_tests/webapollo_specs/fixtures/Group1.33_Amel_4.5.maker.gff';
+	    });
 
-  beforeEach(function() {
-  });
-
-  it("1 should be true", function() {
-	  expect(1).toBeTruthy();
-      });
-
-  /*
-  it("should be able to play a Song", function() {
-    player.play(song);
-    expect(player.currentlyPlayingSong).toEqual(song);
-
-    //demonstrates use of custom matcher
-    expect(player).toBePlaying(song);
-  });
-
-
-  describe("when song has been paused", function() {
-    beforeEach(function() {
-      player.play(song);
-      player.pause();
+	it('should load my fixtures', function () {
+		//var gffString = jasmine.read( "Group1.33_Amel_4.5.maker.gff" );
+		loadFixture( "Group1.33_Amel_4.5.maker.gff" );
+		expect(gffString.toBeDefined() );
+	    });
+	
+	it("1 should be true", function() {
+		expect(1).toBeTruthy();
+	    });
+	
+	it("should respond to parse", function() {
+		expect(gff3Parser.parse).toBeDefined();
+	    });
+	
+	it("should correctly parse Maker GFF3 file to json", function() {
+		var actualJson = gff3Parser.parse( makerGff3 );
+		var expectedJson = [{"parent":
+				     ["Group1.33","maker","gene","245454","247006",".","+",".","ID=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=maker-Group1%252E33-pred_gff_GNOMON-gene-4.137"],
+				     "children": 
+				     [{
+					     "parent": 
+					     ["Group1.33","maker","mRNA","245454","247006",".","+",".","ID=1:gnomon_566853_mRNA;Parent=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=gnomon_566853_mRNA;_AED=0.45;_eAED=0.45;_QI=138|1|1|1|1|1|4|191|259"],
+					     "children": [
+							  ["Group1.33","maker","exon","245454","245533",".","+",".","ID=1:gnomon_566853_mRNA:exon:5976;Parent=1:gnomon_566853_mRNA"],
+							  ["Group1.33","maker","exon","245702","245879",".","+",".","ID=1:gnomon_566853_mRNA:exon:5977;Parent=1:gnomon_566853_mRNA"]]}]}
+		    ];
+		
+		expect(actualJson).toEqual( expectedJson );
+	    });
+	
     });
-
-    it("should indicate that the song is currently paused", function() {
-      expect(player.isPlaying).toBeFalsy();
-
-      // demonstrates use of 'not' with a custom matcher
-      expect(player).not.toBePlaying(song);
-    });
-
-    it("should be possible to resume", function() {
-      player.resume();
-      expect(player.isPlaying).toBeTruthy();
-      expect(player.currentlyPlayingSong).toEqual(song);
-    });
-  });
-
-  // demonstrates use of spies to intercept and test method calls
-  it("tells the current song if the user has made it a favorite", function() {
-    spyOn(song, 'persistFavoriteStatus');
-
-    player.play(song);
-    player.makeFavorite();
-
-    expect(song.persistFavoriteStatus).toHaveBeenCalledWith(true);
-  });
-
-  //demonstrates use of expected exceptions
-  describe("#resume", function() {
-    it("should throw an exception if song is already playing", function() {
-      player.play(song);
-
-      expect(function() {
-        player.resume();
-      }).toThrow("song is already playing");
-    });
-  });
-  */
-
-});
