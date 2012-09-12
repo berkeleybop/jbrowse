36436ada6567634d411fc343db84290cf774d8aa
Justin Reese <justaddcoffee+github@gmail.com>
2012-09-12 18:07:50 +0700

    changed structure of expected JSON again, made returned JSON into a JSON hash, moved parsed data into 'parsedData' attribute, so I can add parse errors and parse warning later

--- tests/js_tests/webapollo_specs/GFF3ParserSpec.js
+++ tests/js_tests/webapollo_specs/GFF3ParserSpec.js
@@ -5,10 +5,12 @@ describe("GFF3toJson", function() {
 	var gff3Parser;
 	var makerGff3String;
 	var makerCorrectJsonFile;
-	
+	var jsonOuput;
+
 	beforeEach(function() {
 		gff3Parser = new GFF3toJson();		
 		makerGff3String = "Group1.33	maker	gene	245454	247006	.	+	.	ID=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=maker-Group1%252E33-pred_gff_GNOMON-gene-4.137;\nGroup1.33	maker	mRNA	245454	247006	.	+	.	ID=1:gnomon_566853_mRNA;Parent=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=gnomon_566853_mRNA;_AED=0.45;_eAED=0.45;_QI=138|1|1|1|1|1|4|191|259;\nGroup1.33	maker	exon	245454	245533	.	+	.	ID=1:gnomon_566853_mRNA:exon:5976;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	exon	245702	245879	.	+	.	ID=1:gnomon_566853_mRNA:exon:5977;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	exon	246046	246278	.	+	.	ID=1:gnomon_566853_mRNA:exon:5978;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	exon	246389	247006	.	+	.	ID=1:gnomon_566853_mRNA:exon:5979;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	five_prime_UTR	245454	245533	.	+	.	ID=1:gnomon_566853_mRNA:five_prime_utr;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	five_prime_UTR	245702	245759	.	+	.	ID=1:gnomon_566853_mRNA:five_prime_utr;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	CDS	245760	245879	.	+	0	ID=1:gnomon_566853_mRNA:cds;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	CDS	246046	246278	.	+	0	ID=1:gnomon_566853_mRNA:cds;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	CDS	246389	246815	.	+	1	ID=1:gnomon_566853_mRNA:cds;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	three_prime_UTR	246816	247006	.	+	.	ID=1:gnomon_566853_mRNA:three_prime_utr;Parent=1:gnomon_566853_mRNA;\n";
+		jsonOutput = gff3Parser.parse( makerGff3String );
 	    });
 
 	it("1 should be true", function() {
@@ -20,31 +22,22 @@ describe("GFF3toJson", function() {
 	    });
 
 	it("should return something non-null", function() {
-		var parsedJson = gff3Parser.parse( makerGff3String );
-		expect(parsedJson).not.toBeNull();
+		expect(jsonOutput).not.toBeNull();
 	    });
 
 	it("should return a parent with the right ID in parsed JSON", function() {
-		var parsedJson = gff3Parser.parse( makerGff3String );
-		expect(parsedJson).toBeDefined();
-		expect(parsedJson[0]).toBeDefined();
-		expect(parsedJson[0]["ID"]).toEqual("maker-Group1%2E33-pred_gff_GNOMON-gene-4.137");
+		expect(jsonOutput["parsedData"][0]).toBeDefined();
+		expect(jsonOutput["parsedData"][0]["ID"]).toEqual("maker-Group1%2E33-pred_gff_GNOMON-gene-4.137");
 	    });
 
 	it("should return a parent with correct data array of 9 element in parsed JSON", function() {
-		var parsedJson = gff3Parser.parse( makerGff3String );
-		expect(parsedJson).toBeDefined();
-		expect(parsedJson[0]).toBeDefined();
-		expect(parsedJson[0]["data"]).toBeDefined();
-		expect(parsedJson[0]["data"].length).toEqual(9);
-		expect(parsedJson[0]["data"] === ["Group1.33","maker","gene","245454","247006",".","+",".","ID=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=maker-Group1%252E33-pred_gff_GNOMON-gene-4.137"]).toBeTruthy;
+		expect(jsonOutput["parsedData"][0]["data"]).toBeDefined();
+		expect(jsonOutput["parsedData"][0]["data"].length).toEqual(9);
+		expect(jsonOutput["parsedData"][0]["data"] === ["Group1.33","maker","gene","245454","247006",".","+",".","ID=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;Name=maker-Group1%252E33-pred_gff_GNOMON-gene-4.137"]).toBeTruthy;
 	    });
 
 	it("should return children in parsed JSON", function() {
-		var parsedJson = gff3Parser.parse( makerGff3String );
-		expect(parsedJson).toBeDefined();
-		expect(parsedJson[0]).toBeDefined();
-		expect(parsedJson[0]["children"]).toBeDefined();
+		expect(jsonOutput["parsedData"][0]["children"]).toBeDefined();
 	    });
 
 	/*
