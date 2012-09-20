07d47cbe3b8dff5cc5521c79138e4a355958cd43
Justin Reese <justaddcoffee+github@gmail.com>
2012-09-20 13:26:50 +0700

    trivial syntax change to GFF3 parser test

--- tests/js_tests/webapollo_specs/GFF3ParserSpec.js
+++ tests/js_tests/webapollo_specs/GFF3ParserSpec.js
@@ -9,18 +9,8 @@ describe("GFF3toJson", function() {
 
 	// variables for holding fixtures and parsed output
 	var gff3Parser;
-	var makerGff3String;
-	var makerGff3String2;
-	var makerGff3String3;
-	var makerGff3String4;
-	var makerGff3String5;
-	var makerGff3String6;
-	var jsonOutput;
-	var jsonOutput2;
-	var jsonOutput3;
-	var jsonOutput4;
-	var jsonOutput5;
-	var jsonOutput6;
+	var makerGff3String, makerGff3String2, makerGff3String3, makerGff3String4, makerGff3String5, makerGff3String6;
+	var jsonOutput, jsonOutput2, jsonOutput3, jsonOutput4, jsonOutput5, jsonOutput6;
 
 	beforeEach(function() {
 		gff3Parser = new GFF3toJson();		
@@ -41,7 +31,7 @@ describe("GFF3toJson", function() {
 		jsonOutput5 = gff3Parser.parse( makerGff3String5 );
 
 		// here's a fixture with great granchildren. doesn't necessarily make sense biologically, just using this to test parsing of deep features.
-		// the deepest I'm going to test is great grandchildren. 
+		// the deepest I'm going to test is great grandchildren.  
 		makerGff3String6 = "Group1.33	maker	gene	245454	247006	.	+	.	ID=this_parent_id_12345;Name=maker-Group1%2E33-pred_gff_GNOMON-gene-4.137;\nGroup1.33	maker	mRNA	245454	247006	.	+	.	ID=1:gnomon_566853_mRNA;Parent=this_parent_id_12345;Name=gnomon_566853_mRNA;_AED=0.45;_eAED=0.45;_QI=138|1|1|1|1|1|4|191|259;\nGroup1.33	maker	exon	245454	245533	.	+	.	ID=1:gnomon_566853_mRNA:exon:5976;Parent=1:gnomon_566853_mRNA;\nGroup1.33	maker	three_prime_UTR	246816	247006	.	+	.	ID=1:gnomon_566853_mRNA:three_prime_utr;Parent=1:gnomon_566853_mRNA:exon:5976;\nGroup1.33	maker	TF_binding_site	246816	246820	.	+	.	ID=1:gnomon_566853_TFBS;Parent=1:gnomon_566853_mRNA:three_prime_utr;";
 
 		jsonOutput6 = gff3Parser.parse( makerGff3String6 );
