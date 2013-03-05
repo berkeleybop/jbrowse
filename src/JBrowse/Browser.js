d19bbd8a59d73f3e0dc784b3452fafbe9b65e95c
gregg <gregg@68-a8-6d-3-1d-80.dhcp.lbnl.us>
2013-03-05 14:46:19 -0800

    Fixes issue #216 and issue #217 in GMOD/jbrowse: #216: Navigating via sequence dropdown selection ignores previous location on selected sequence, whereas with navigation text box entering just a sequence name navigates to previous location on that sequence (if visited previously). #217: Initial default view of previously unseen seq starts at 80% centered view, but any use of seq pulldown takes it to 100%

--- src/JBrowse/Browser.js
+++ src/JBrowse/Browser.js
@@ -1979,7 +1979,9 @@ Browser.prototype.createNavBox = function( parent ) {
                     return { label: refseqName, value: refseqName };
                 }),
                 onChange: dojo.hitch(this, function( newRefName ) {
-                    this.navigateToLocation({ ref: newRefName }); 
+                    if (newRefName !== this.refSeq.name) {  //  only trigger navigation if actually switching sequences
+                        this.navigateTo(newRefName);
+                    }
                 })
             }).placeAt( refSeqSelectBoxPlaceHolder );
         }
